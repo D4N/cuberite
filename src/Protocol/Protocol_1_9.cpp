f08558665fe1efcb2907b3c6ff6dc75c6d5614d0
Lukas Pioch <lukas@zgow.de>
2017-07-31 10:45:52 +0200

    Added check if the player is holding a book and use SendEquippedSlot to send the item change

--- src/Protocol/Protocol_1_9.cpp
+++ src/Protocol/Protocol_1_9.cpp
@@ -2908,6 +2908,13 @@ void cProtocol_1_9_0::HandleVanillaPluginMessage(cByteBuffer & a_ByteBuffer, con
 			return;
 		}
 
+		cPlayer & Player = *m_Client->GetPlayer();
+		if (Player.GetEquippedItem().m_ItemType != E_ITEM_BOOK_AND_QUILL)
+		{
+			// Equipped item is not a writeable book
+			return;
+		}
+
 		// Skip item count (1 byte) and item damage (2 bytes)
 		a_ByteBuffer.SkipRead(3);
 
@@ -2929,8 +2936,6 @@ void cProtocol_1_9_0::HandleVanillaPluginMessage(cByteBuffer & a_ByteBuffer, con
 			cBookContent::ParseFromNBT(0, BookItem.m_BookContent, NBT);
 		}
 
-		cPlayer & Player = *m_Client->GetPlayer();
-
 		// If true, player has clicked on the sign button
 		bool IsSigned = (BookItem.m_ItemType == E_ITEM_WRITTEN_BOOK) ? true : false;
 
@@ -2945,7 +2950,7 @@ void cProtocol_1_9_0::HandleVanillaPluginMessage(cByteBuffer & a_ByteBuffer, con
 
 		cInventory & inv = Player.GetInventory();
 		inv.SetHotbarSlot(inv.GetEquippedSlotNum(), BookItem);
-		SendWholeInventory(*Player.GetWindow());  // TODO: Use SendSlot
+		Player.GetInventory().SendEquippedSlot();
 		return;
 	}
 	LOG("Unhandled vanilla plugin channel: \"%s\".", a_Channel.c_str());
